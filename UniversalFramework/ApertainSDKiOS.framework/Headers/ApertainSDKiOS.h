e1d8a5710b98de03fa337ff7d78c6f4aba74820a
jkltech <jay@jkltech.in>
2017-01-27 15:40:33 +0530

    Initial Commit, will be updated.

---
+++ UniversalFramework/ApertainSDKiOS.framework/Headers/ApertainSDKiOS.h
@@ -0,0 +1,20 @@
+//
+//  ApertainSDKiOS.h
+//  ApertainSDKiOS
+//
+//  Created by JKL Technologies on 19/01/17.
+//  Copyright © 2017 JKL Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FMDatabase.h"
+#import "FBEncryptorAES.h"
+//! Project version number for ApertainSDKiOS.
+FOUNDATION_EXPORT double ApertainSDKiOSVersionNumber;
+
+//! Project version string for ApertainSDKiOS.
+FOUNDATION_EXPORT const unsigned char ApertainSDKiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApertainSDKiOS/PublicHeader.h>
+
+
